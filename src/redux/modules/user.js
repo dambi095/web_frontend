700a0766de453d6ea448b978702f9d053faabf3a
dambya <qwerty6789011@gmail.com>
2019-11-30 00:55:19 +0900

    redux 라이브러리 추가

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -8,8 +8,7 @@
 
 const initialState = {
     // localStorage : 브라우저에 저장하는 쿠키 
-    // JWT 아이템이 로컬 스토리지 안에 있는지 체크, JWT가 없을경우 false
-    // 만약 유저 토큰이 존재한다면 이는 로그인 되었다는 뜻
+    // JWT 아이템이 로컬 스토리지 안에 있는지 체크, JWT가 없을경우 false, 유저 토큰이 존재한다면 이는 로그인 되었다는 뜻
     isLoggedIn: localStorage.getItem("jwt") || false
 }
 
