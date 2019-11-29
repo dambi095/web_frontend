8fb60c1f9da07541cc985577be30419cb3860869
dambya <qwerty6789011@gmail.com>
2019-11-30 02:10:34 +0900

    NODE_ENV 상태에 따라 redux를 위해한 작업들을 log 하도록 추가

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,23 @@
 
-// configurestore 에서는 스토어를 설정 및 구성해준다.
+/*
+ *  configurestore 에서는 스토어를 설정 및 구성해준다.
+ */
 
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import users from "./modules/user";
 
+// prod or dev 에 따라 스토어의 설정이 달라진다. 
+const env = process.env.NODE_ENV;
+
 // 미들웨어 생성 
 const middlewares = [thunk];
+
+if (env === "development") {
+    // 리덕스를 위해 한 작업들을 log 한다. 즉 이전 state 나 액션, 액션의 결과를 보고싶을 때 유용 
+    const { logger } = require("redux-logger");
+    middlewares.push(logger);
+}
 
 // 여러 리듀서를 합치는 부분 
 const reducer = combineReducers({
@@ -14,8 +25,8 @@ const reducer = combineReducers({
 })
 
 
-let store = initialState => 
-// ...middlewares 로 작성하면 array를 풀 수 있다.
-createStore(reducer, applyMiddleware(...middlewares));
+let store = initialState =>
+    // ...middlewares 로 작성하면 array를 풀 수 있다.
+    createStore(reducer, applyMiddleware(...middlewares));
 
 export default store();
