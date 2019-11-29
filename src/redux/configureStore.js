30ef6278faee7c03c68db2a19eac23088f694032
dambya <qwerty6789011@gmail.com>
2019-11-30 01:53:40 +0900

    thunk 미들웨어 적용

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,21 @@
 
-// configurestore 에서는 나의 스토어를 설정 및 구성
+// configurestore 에서는 스토어를 설정 및 구성해준다.
 
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import users from "./modules/user";
+
+// 미들웨어 생성 
+const middlewares = [thunk];
 
 // 여러 리듀서를 합치는 부분 
 const reducer = combineReducers({
     users
 })
 
-let store = initialState => createStore(reducer);
+
+let store = initialState => 
+// ...middlewares 로 작성하면 array를 풀 수 있다.
+createStore(reducer, applyMiddleware(...middlewares));
 
 export default store();
