810d932e6e8d665024b079c84770596c0c6449d6
dambya <qwerty6789011@gmail.com>
2019-11-30 00:51:13 +0900

    basic reducer 틀 생성

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -7,13 +7,23 @@
 // initial state
 
 const initialState = {
-    isLoggedIn: localStorage // 브라우저에 저장하는 쿠키 
-} 
+    // localStorage : 브라우저에 저장하는 쿠키 
+    // JWT 아이템이 로컬 스토리지 안에 있는지 체크, JWT가 없을경우 false
+    // 만약 유저 토큰이 존재한다면 이는 로그인 되었다는 뜻
+    isLoggedIn: localStorage.getItem("jwt") || false
+}
 
 // reducer
 
+function reducer(state = initialState, action) {
+    switch (action, type) {
+        default:
+            return state;
+    }
+}
 // reducer funtions
 
 // exports
 
 // reducer export 
+export default reducer;
