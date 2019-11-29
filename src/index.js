cfba54ac0d20d6946e4b9db7adce93e11113ad00
dambya <qwerty6789011@gmail.com>
2019-11-30 03:25:40 +0900

    리듀서, routing 네비게이션, 히스토리 싱크 및 react router 버전 업데이트로 생긴 이슈로 인한 코드수정 및 connect-react-router 라이브러리 추가

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from '../src/redux/configureStore';
-import App from './App';
+import { ConnectedRouter } from 'connected-react-router';
+import store, { history } from '../src/redux/configureStore';
+// import App from './App';
 
 console.log(store.getState());
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
-);
+function App() {
+    return (
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <>
+            <h1>Hello CodeSandbox</h1>
+            <h2>Start editing to see some magic happen!</h2>
+          </>
+        </ConnectedRouter>
+      </Provider>
+    );
+  }
+  
+  const rootElement = document.getElementById("root");
+  ReactDOM.render(<App />, rootElement);
+  
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,27 +1,34 @@
 
 /*
- *  configurestore 에서는 스토어를 설정 및 구성해준다.
+  configurestore 에서는 스토어를 설정 및 구성해준다.
  */
-
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { routerMiddleware, connectRouter } from "connected-react-router";
+import { createBrowserHistory } from "history";
 import users from "./modules/user";
 
 // prod or dev 에 따라 스토어의 설정이 달라진다. 
 const env = process.env.NODE_ENV;
 
-// 미들웨어 생성 
-const middlewares = [thunk];
+const history = createBrowserHistory();
+
+/*
+    필요한 미들웨어 생성
+    routerMiddleware: 히스토리랑 싱크되어야한다.
+*/
+const middlewares = [thunk, routerMiddleware(history)];
 
 if (env === "development") {
-    // 리덕스를 위해 한 작업들을 log 한다. 즉 이전 state 나 액션, 액션의 결과를 보고싶을 때 유용 
+    // 리덕스를 위해 한 작업들을 log. 즉 이전 state 나 액션, 액션의 결과를 보고싶을 때 유용 
     const { logger } = require("redux-logger");
     middlewares.push(logger);
 }
 
-// 여러 리듀서를 합치는 부분 
+// 여러개의 리듀서를 합치는 부분 
 const reducer = combineReducers({
-    users
+    users,
+    router: connectRouter(history) // routing: 불러온 router reducer
 })
 
 
@@ -29,4 +36,6 @@ let store = initialState =>
     // ...middlewares 로 작성하면 array를 풀 수 있다.
     createStore(reducer, applyMiddleware(...middlewares));
 
+// history를 export 하는 이유는 곧 생성될 라우터에겐 히스토리 오브젝트가 필요하기 때문이다.
+export { history };
 export default store();
