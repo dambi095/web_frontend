58bf22649c3040993e6b3518ad9ee3be152854d6
dambya <qwerty6789011@gmail.com>
2019-11-29 22:20:12 +0900

    폴더 구조 및 redux 구조 설정 중

---
+++ src/redux/modules/user.js
@@ -0,0 +1,19 @@
+// imports
+
+// actions
+
+// action creators
+
+// initial state
+
+const initialState = {
+    isLoggedIn: localStorage // 브라우저에 저장하는 쿠키 
+} 
+
+// reducer
+
+// reducer funtions
+
+// exports
+
+// reducer export 
