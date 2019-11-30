efc5e7b7ee6af7cea2d0598841d4875d930d547c
dambya <qwerty6789011@gmail.com>
2019-11-30 23:05:01 +0900

    불필요한 파일 제거 및 Wrapper css 추가

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
-import styles from "./styles.scss";
+import styled, { ThemeProvider } from "styled-components";
+import { HashRouter as Router } from "react-router-dom";
+import GlobalStyles from "../../styles/GlobalStyles";
+import Theme from "../../styles/Theme";
+
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: ${props => props.theme.maxWidth};
+  width: 100%;
+`;
 
 class App extends Component {
     render() {
         return (
-            <div className={styles.App}>
-                <h1>Welcome</h1>
-            </div>
+            <ThemeProvider theme={Theme}>
+                <>
+                    <GlobalStyles />
+                    <Router>
+                        <Wrapper>
+                            <h2>Hello </h2>
+                        </Wrapper>
+                    </Router>
+                </>
+            </ThemeProvider>
         )
     }
 }
