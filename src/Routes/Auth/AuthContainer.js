ee688eb4c1d0d96bd7621875806aca1bc7f40d72
dambya <qwerty6789011@gmail.com>
2019-12-04 01:11:51 +0900

    logIn화면 일부 생성

--- src/Routes/Auth/AuthContainer.js
+++ src/Routes/Auth/AuthContainer.js
@@ -5,5 +5,32 @@ import { toast } from "react-toastify";
 
 
 export default () => {
+    const [action, setAction] = useState("logIn");
+    const username = useInput("");
+    const firstName = useInput("");
+    const lastName = useInput("");
+    const email = useInput("");
+    const secret = useInput("");
 
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        if (action === "logIn") {
+
+        } else if (action === "signUp") {
+
+        } else if (action === "confirm") {
+
+        }
+    }
+
+    return <AuthPresenter
+        setAction={setAction}
+        action={action}
+        username={username}
+        firstName={firstName}
+        lastName={lastName}
+        email={email}
+        onSubmit={onSubmit}
+        secret={secret}
+    />
 }
